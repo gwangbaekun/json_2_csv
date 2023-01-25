c1856e13390849f8398e70d38fd4c5efe7803689
Jeyeol BAEK <gwangbaekun@gmail.com>
2023-01-25 16:05:57 +0900

    json to csv

--- json_2_csv.js
+++ json_2_csv.js
@@ -1,19 +1,26 @@
 // this module is made for converting json to csv
 // not for converting csv to json
 
-fetch("./data.json")
-  .then((response) => response.json())
-  .then((json) => console.log(json));
+// custom field----------------------------------------------------
+const fieldToConvert = "id";
+const fileToRead = "./data.json";
 
-// const replacer = (key, value) => (value === null ? "" : value); // specify how you want to handle null values here
-// const header = Object.keys(items[0]);
-// const csv = [
-//   header.join(","), // header row first
-//   ...items.map((row) =>
-//     header
-//       .map((fieldName) => JSON.stringify(row[fieldName], replacer))
-//       .join(",")
-//   ),
-// ].join("\r\n");
+json = require(fileToRead);
+const items = json.customers;
+// ----------------------------------------------------------------
 
-// console.log(csv);
+const replacer = (key, value) => (value === null ? "" : value); // specify how you want to handle null values here
+const header = Object.keys(items[0]);
+const csv = [
+  header.join(","), // header row first
+  ...items.map((row) =>
+    header
+      .map((fieldName) => JSON.stringify(row[fieldName], replacer))
+      .join(",")
+  ),
+].join("\r\n");
+
+const specificField = items.map((row) => row[fieldToConvert]);
+
+console.log(csv);
+console.log("specificField", specificField);
